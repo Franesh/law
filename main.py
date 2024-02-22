ebeb0340df63157a98db20300b4d3e1484cdd2c1
Franesh <126826199+Franesh@users.noreply.github.com>
2024-02-22 13:30:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -109,9 +109,4 @@ def home():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
-    if __name__ == '__main__':
-        with app.app_context():
-            db.drop_all()  # Drop all tables
-            db.create_all()  # Create all tables
-        app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0", port=8080)
